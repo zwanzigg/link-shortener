5e4d8caed20b6508ce20cb05ddf718c07b24d80f
zwanzigg <verashreder@gmail.com>
2023-07-05 16:59:42 +0200

    Set up dynamic DB host name depending on the node environment

--- server/src/ormconfig.ts
+++ server/src/ormconfig.ts
@@ -1,9 +1,11 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm/dist/interfaces/typeorm-options.interface";
 import { Link } from "./links/link.entity";
 
+const NODE_ENV = process.env.NODE_ENV || "development";
+
 export const ORMConfig: TypeOrmModuleOptions = {
   type: "mysql",
-  host: "db",
+  host: NODE_ENV === "development" ? "localhost" : "db",
   port: 3306,
   username: "root",
   password: "root",
